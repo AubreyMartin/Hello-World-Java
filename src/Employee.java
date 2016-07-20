590369d4eccb74aed740c94198d342411622d580
AUBREY PC <AUBREY PC@Admin-PC>
2016-07-20 10:20:29 +0530

    Added Employee class

---
+++ src/Employee.java
@@ -0,0 +1,41 @@
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String officeCity;
+	private int id;
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	public String getFirstName(){
+		return this.firstName;
+	}
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+	public String getLastName(){
+		  return this.lastName;
+	  }
+    public void  setAge(int age){
+    	this.age = age ;
+    }
+    public int getAge(){
+    	return this.age;
+    }
+    public void setOfficeCity(String officeCity){
+    	this.officeCity = officeCity;
+    }
+    public String getOfficecity(){
+    	return this.officeCity;
+    }
+    public void setId(int id){
+    	this.id = id;
+    }
+     public int getId(){
+    	 return this.id;
+    }
+}
+//LastName
+//age
+//oficeLocation
+//id
